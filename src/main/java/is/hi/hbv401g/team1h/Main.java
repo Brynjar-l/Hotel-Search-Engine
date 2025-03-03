ca2725f48b5dc06ef295e26b83266d584e5b07f1
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-03-03 09:28:22 +0000

    init

---
+++ src/main/java/is/hi/hbv401g/team1h/Main.java
@@ -0,0 +1,10 @@
+package is.hi.hbv401g.team1h;
+
+
+import org.jdbi.v3.core.Jdbi;
+
+public class Main {
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/hbv401g");
+    }
+}
