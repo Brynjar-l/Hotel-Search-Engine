9e9e8fc8eee1d6ec248d7e2eaf45daf66287d7a6
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-03-06 16:01:52 +0000

    added to SearchService.java. Created QueryObject.java

--- src/main/java/is/hi/hbv401g/team1h/service/SearchService.java
+++ src/main/java/is/hi/hbv401g/team1h/service/SearchService.java
@@ -1,4 +1,95 @@
 package is.hi.hbv401g.team1h.service;
 
+import is.hi.hbv401g.team1h.model.Amenity;
+import is.hi.hbv401g.team1h.model.Hotel;
+import is.hi.hbv401g.team1h.utils.QueryObject;
+
+import java.time.LocalDate;
+import java.util.*;
+
 public class SearchService {
+    private QueryObject currentQuery;
+
+
+    public SearchService() {
+        newQuery();
+    }
+
+
+    public void newQuery() {
+        this.currentQuery = new QueryObject();
+    }
+
+
+
+    // call once: amenity added, call again: amenity removed, call again: amenity added, o.s.frv.
+    public void toggleAmenity(String filter) {
+        // convert
+    }
+    public void toggleAmenity(Amenity amenity) {
+        currentQuery.addClause(amenity);
+    }
+
+
+
+    public void setPriceRange(int lowerBound, int upperBound) {
+
+    }
+    public void setPriceLowerBound(int lowerBound) {
+
+    }
+    public void setPriceUpperBound(int upperBound) {
+
+    }
+
+
+
+    public void setDateRange(LocalDate fromDate, LocalDate toDate) {
+
+    }
+    public void setDateFrom(LocalDate fromDate) {
+
+    }
+    public void setDateTo(LocalDate toDate) {
+
+    }
+
+    private void errorCheck() {
+
+        // if neither date was modified, show all future available dates
+        if (currentQuery.getFromDate() == null && currentQuery.getToDate() == null) {
+            currentQuery.setDateRange(LocalDate.now(), LocalDate.MAX);
+        }
+
+        // if one date, but not both, was modified then throw an exception.
+        if (currentQuery.getFromDate() == null && currentQuery.getToDate() != null
+                        || currentQuery.getFromDate() != null && currentQuery.getToDate() == null) {
+            throw new RuntimeException("Must Select both from and to date");
+        }
+
+        // if fromDate comes after toDate, throw runtime exception
+        if (currentQuery.getFromDate().isAfter(currentQuery.getToDate())) {
+            throw new RuntimeException("FromDate must come before To Date");
+        }
+
+        // if both price settings are untouched, default to the entire range
+        if (currentQuery.getPriceLowerBound() == null || currentQuery.getPriceUpperBound() == null) {
+            currentQuery.setPriceLowerBound(0);
+            currentQuery.setPriceUpperBound(Integer.MAX_VALUE);
+        }
+
+
+
+    }
+
+
+    public List<Hotel> search() {
+        errorCheck();
+
+
+
+        return null;
+    }
 }
+
+
---
+++ src/main/java/is/hi/hbv401g/team1h/utils/QueryObject.java
@@ -0,0 +1,96 @@
+package is.hi.hbv401g.team1h.utils;
+
+import is.hi.hbv401g.team1h.model.Amenity;
+import is.hi.hbv401g.team1h.model.Hotel;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class QueryObject {
+
+    Set<Amenity> clauseList;    // no duplicates
+
+    Integer priceLowerBound;
+    Integer priceUpperBound;
+
+    LocalDate fromDate;
+    LocalDate toDate;
+
+
+    public QueryObject() {
+        clauseList = new HashSet<Amenity>();
+    }
+
+    public List<Hotel> execute() {
+        if (fromDate == null || toDate == null) {
+            throw new IllegalArgumentException("fromDate and toDate are required");
+        }
+        if (fromDate.isAfter(toDate)) {
+            throw new IllegalArgumentException("fromDate is after toDate");
+        }
+        if (priceLowerBound == null || priceUpperBound == null ) {
+            throw new IllegalArgumentException("priceLowerBound and priceUpperBound are required");
+        }
+        if (priceLowerBound > priceUpperBound) {
+            throw new IllegalArgumentException("priceLowerBound > priceUpperBound");
+        }
+
+        // clause list, no forget
+        return null; // dao call and return list
+    }
+
+
+
+
+    public void setFromDate(LocalDate fromDate) {
+        this.fromDate = fromDate;
+    }
+    public void setToDate(LocalDate toDate) {
+        this.toDate = toDate;
+    }
+    public void setDateRange(LocalDate fromDate, LocalDate toDate) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+    }
+    public LocalDate getFromDate() {
+        return fromDate;
+    }
+    public LocalDate getToDate() {
+        return toDate;
+    }
+
+
+
+    public void setPriceLowerBound(int priceLowerBound) {
+        this.priceLowerBound = priceLowerBound;
+    }
+    public void setPriceUpperBound(int priceUpperBound) {
+        this.priceUpperBound = priceUpperBound;
+    }
+    public void setPriceRange(int priceLowerBound, int priceUpperBound) {
+        this.priceLowerBound = priceLowerBound;
+        this.priceUpperBound = priceUpperBound;
+    }
+    public Integer getPriceLowerBound() {
+        return priceLowerBound;
+    }
+    public Integer getPriceUpperBound() {
+        return priceUpperBound;
+    }
+
+
+
+    public void addClause(Amenity clause) {     // does not throw an exception on duplicate input
+        clauseList.add(clause);
+    }
+    public void removeClause(Amenity clause) {  // does not throw an exception if element does not exist in list
+        clauseList.remove(clause);
+    }
+
+    public List<Amenity> getClauseList() {
+        return new ArrayList<>(clauseList);
+    }
+}
