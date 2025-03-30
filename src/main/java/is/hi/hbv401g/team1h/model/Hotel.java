e75a40a7e12a3285cba944d4c78d5fe779bf467f
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-03-30 17:24:46 +0000

    minor tweak

--- src/main/java/is/hi/hbv401g/team1h/model/Hotel.java
+++ src/main/java/is/hi/hbv401g/team1h/model/Hotel.java
@@ -33,4 +33,53 @@ public class Hotel {
         this.starRating = starRating;
         this.description = description;
     }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(double starRating) {
+        this.starRating = starRating;
+    }
 }
