2abbde93b89721d9343e6f6d15c6b5cc34b903bc
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-03-30 16:13:22 +0000

    test init

---
+++ src/test/java/is/hi/hbv401g/team1h/service/MainServiceTest.java
@@ -0,0 +1,19 @@
+package is.hi.hbv401g.team1h.service;
+
+
+import org.junit.jupiter.api.Test;
+
+class MainServiceTest {
+
+    @Test
+    void getAllHotels() {
+    }
+
+    @Test
+    void createNewHotel() {
+    }
+
+    @Test
+    void getHotelById() {
+    }
+}
