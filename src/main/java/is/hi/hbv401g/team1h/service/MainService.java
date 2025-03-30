88ec6c4c334858f84dae1c63631824365d9646ff
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-03-30 16:06:16 +0000

    main

--- src/main/java/is/hi/hbv401g/team1h/service/MainService.java
+++ src/main/java/is/hi/hbv401g/team1h/service/MainService.java
@@ -1,4 +1,17 @@
 package is.hi.hbv401g.team1h.service;
 
+import is.hi.hbv401g.team1h.model.Hotel;
+
+import java.util.List;
+
 public class MainService {
+
+
+    public List<Hotel> getAllHotels() { return null; }
+    public void createNewHotel() {  }
+
+
+    public Hotel getHotelById(int id) { return null; }
+
+
 }
