365d08a2b22541dd3616185c19bf8e8258912f2a
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-03-30 16:22:54 +0000

    test init

--- src/test/java/is/hi/hbv401g/team1h/service/MainServiceTest.java
+++ src/test/java/is/hi/hbv401g/team1h/service/MainServiceTest.java
@@ -1,9 +1,24 @@
 package is.hi.hbv401g.team1h.service;
 
 
+import is.hi.hbv401g.team1h.utils.JdbiManager;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class MainServiceTest {
+
+    @BeforeAll
+    static void setup() {
+        Jdbi jdbi = Jdbi.create("jdbc:sqlite::memory:");
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        JdbiManager.populate(jdbi);
+        // -> add rooms to roomsTable
+        // -> add bookings to bookingsTable
+
+    }
 
     @Test
     void getAllHotels() {
